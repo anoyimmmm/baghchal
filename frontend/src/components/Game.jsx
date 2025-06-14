c185091cbc558df0e956196a21439849d8ce974a
Bipin Thapa <tailung12345678910@gmail.com>
2025-06-14 09:23:21 +0545

    add dummy websocket

--- frontend/src/components/Game.jsx
+++ frontend/src/components/Game.jsx
@@ -1,18 +1,18 @@
 import Board from "./Board";
 const Game = () => {
   const handleMoveSend = (message) => {
-    console.log(message);
-    // const url = `ws://${window.location.host}/game`;
-    // const socket = new WebSocket(url);
-    // // open a websocket
-    // socket.addEventListener("open", (event) => {
-    //   socket.send(message);
-    //   console.log("message sent");
-    // });
+    // console.log(message);
+    const url = `ws://${window.location.host}/game`;
+    const socket = new WebSocket(url);
+    // open a websocket
+    socket.addEventListener("open", (event) => {
+      socket.send(message);
+      console.log("message sent");
+    });
 
-    // socket.addEventListener("message", (event) => {
-    //   console.log("Message from server ", event.data);
-    // });
+    socket.addEventListener("message", (event) => {
+      console.log("Message from server ", event.data);
+    });
   };
 
   return (
