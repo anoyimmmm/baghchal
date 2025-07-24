da383828417ecc2a3d7a69343c5957e64e874cd0
Bipin Thapa <tailung12345678910@gmail.com>
2025-07-24 23:38:23 +0545

    add gameover modal (incomplete)

--- frontend/src/components/Game.jsx
+++ frontend/src/components/Game.jsx
@@ -1,9 +1,26 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, use } from "react";
 import Board from "./Board";
+const initialGameState = {
+  board: {
+    "0-0": "tiger",
+    "0-4": "tiger",
+    "4-0": "tiger",
+    "4-4": "tiger",
+  },
+  status: "ongoing",
+  currentPlayer: "goat",
+  phase: "placement",
+  unusedGoat: 20,
+  deadGoatCount: 0,
+  status: "ongoing",
+  winner: null,
+};
 
 const Game = () => {
   const socketRef = useRef();
   const [gameState, setGameState] = useState();
+  const [modalOpen, setModalOpen] = useState(false);
+  const [winner, setWinner] = useState("");
 
   useEffect(() => {
     const wsUrl = import.meta.env.VITE_WS_URL;
@@ -15,8 +32,13 @@ const Game = () => {
 
     socketRef.current.onmessage = (event) => {
       const new_game_state = JSON.parse(event.data).message.game_state;
-      console.log(new_game_state);
+      // console.log(new_game_state);
       setGameState(new_game_state);
+      if (new_game_state.status !== "ongoing") {
+        console.log("hello world");
+        setWinner(new_game_state.winner);
+        setModalOpen(true);
+      }
     };
 
     socketRef.current.onclose = () => {
@@ -45,8 +67,38 @@ const Game = () => {
           onMoveSend={handleMoveSend}
         />
       )}
+      <WinnerModal
+        winner={winner}
+        isOpen={modalOpen}
+        // onclose={() => setModalOpen(false)}
+      />
     </>
   );
 };
 
 export default Game;
+
+function WinnerModal({ winner, isOpen, onClose }) {
+  if (!isOpen) return null;
+
+  return (
+    <div
+      className="fixed inset-0  flex items-center justify-center z-50
+"
+      onClick={(e) => {
+        if (e.target === e.currentTarget) onClose();
+      }}
+    >
+      <div className="bg-gray-800 rounded-lg shadow-lg max-w-md w-full p-6 text-center text-white">
+        <h2 className="text-2xl font-bold mb-4">🎉 Congratulations!</h2>
+        <p className="mb-6 text-lg">{winner} is the winner!</p>
+        <button
+          onClick={onClose}
+          className="bg-green-600 hover:bg-green-700 px-4 py-2 rounded text-white font-semibold"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  );
+}
